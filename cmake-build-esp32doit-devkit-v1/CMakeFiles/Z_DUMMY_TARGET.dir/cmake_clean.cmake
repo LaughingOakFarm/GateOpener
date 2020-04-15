def16655bddb21cb75c8e8880d325f9523a4056f
Tyler Jensen <ronsper@gmail.com>
2020-04-14 20:24:48 -0700

    Added build support files

---
+++ cmake-build-esp32doit-devkit-v1/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/esp32doit-devkit-v1/ACS712_Current_Sensor_ID1601/src/ACS712.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/esp32doit-devkit-v1/Adafruit_ADS1X15_ID342/Adafruit_ADS1015.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/esp32doit-devkit-v1/ESP32_AnalogWrite_ID5819/src/analogWrite.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/esp32doit-devkit-v1/RunningAverage_ID1322/RunningAverage.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/GateController.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/GateMotor.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/LightSensor.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/Log.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/Main.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/SensorModule.cpp.obj"
+  "Z_DUMMY_TARGET"
+  "Z_DUMMY_TARGET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
